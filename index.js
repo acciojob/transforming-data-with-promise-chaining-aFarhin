d735e9685cd87bb295bf58c793b53176987c0db9
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-04-09 00:00:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,71 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/main.html'));
 });
 //your code here
+
+
+const input = document.getElementById('ip');
+const button = document.getElementById('btn');
+const output = document.getElementById('output');
+
+button.addEventListener('click', () => {
+  const number = input.value;
+  Promise.resolve(number)
+    .then((number) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(number);
+        }, 2000);
+      });
+    })
+    .then((number) => {
+      output.textContent = `Result: ${number}`;
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(number);
+        }, 2000);
+      });
+    })
+    .then((number) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(number * 2);
+        }, 1000);
+      });
+    })
+    .then((number) => {
+      output.textContent = `Result: ${number}`;
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(number - 3);
+        }, 1000);
+      });
+    })
+    .then((number) => {
+      output.textContent = `Result: ${number}`;
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(number / 2);
+        }, 1000);
+      });
+    })
+    .then((number) => {
+      output.textContent = `Result: ${number}`;
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(number + 10);
+        }, 1000);
+      });
+    })
+    .then((number) => {
+      output.textContent = `Final Result: ${number}`;
+    })
+    .catch((error) => {
+      output.textContent = `Error: ${error.message}`;
+    });
+});
+
+
+
 app.post('/add', (req, res) => {
   const {a,b} = req.body;
   res.status(200).send(a+b);
